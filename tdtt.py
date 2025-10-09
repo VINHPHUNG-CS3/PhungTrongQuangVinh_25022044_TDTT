ef80b6a238d8ee18b12ef3fbb62ce260e0dbbefb
VINHPHUNG-CS3 <jaoshingandfm1206@gmail.com>
2025-10-09 10:42:22 +0700

    Nộp code TH4 lần 2

--- tdtt.py
+++ tdtt.py
@@ -1,1 +1,13 @@
-print( int(2.5))
+def check(x):
+    flag = True
+    if x < 2:
+        flag = False
+    for i in range (2, int(x**0.5) + 1):
+        if x % i == 0:
+            flag = False
+            break
+    return flag
+
+a,b = map(int, input().split())
+
+print(check(2), check(3))
